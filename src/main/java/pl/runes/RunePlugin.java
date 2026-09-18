625385006e1d972ce8ecebb3edf36711fed15bc7
muciokrysz-67 <muciokrysz@gmail.com>
2026-09-18 22:12:19 +0200

    Update RunePlugin.java

--- src/main/java/pl/runes/RunePlugin.java
+++ src/main/java/pl/runes/RunePlugin.java
@@ -1,5 +1,6 @@
 package pl.runes;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RunePlugin extends JavaPlugin {
@@ -13,10 +14,16 @@ public class RunePlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new RuneListener(manager), this);
 
         RuneCommand command = new RuneCommand(manager);
-        getCommand("rune").setExecutor(command);
-        getCommand("rune").setTabCompleter(command);
+        PluginCommand rune = getCommand("rune");
+        if (rune == null) {
+            getLogger().severe("Komenda /rune nie jest zarejestrowana w plugin.yml!");
+        } else {
+            rune.setExecutor(command);
+            rune.setTabCompleter(command);
+        }
 
         manager.startTasks();
+        getLogger().info("PotteryRunes wlaczony (v" + getDescription().getVersion() + ")");
     }
 
     @Override
