08ee88b4e0082e587ff4ec9b47d1fe0e7a6fa95c
muciokrysz-67 <muciokrysz@gmail.com>
2026-09-18 22:23:47 +0200

    Update RuneManager.java

--- src/main/java/pl/runes/RuneManager.java
+++ src/main/java/pl/runes/RuneManager.java
@@ -1,6 +1,6 @@
 package pl.runes;
 
-import net.md_5.bungee.api.chat.ChatMessageType;
+import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
